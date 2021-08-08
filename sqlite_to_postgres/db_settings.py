51d17621b1e90ac2df21805223742308578aad84
felki <f3lki@ya.ru>
2021-08-08 20:16:31 +0300

    load data

---
+++ sqlite_to_postgres/db_settings.py
@@ -0,0 +1,8 @@
+dsl = {
+    "dbname": "",
+    "user": "",
+    "password": "",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "options": f"-c search_path=content",
+}
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,42 @@
+import csv
 import sqlite3
-
 import psycopg2
+from db_settings import dsl
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
+from os import remove
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
+def save_csv(file_name: str, data: list):
+    """Сохранение csv файла."""
 
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+    with open(f"{file_name}.csv", "w") as f:
+        writer = csv.writer(f, delimiter="|")
+        writer.writerows(data)
 
 
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'postgres', 'password': 1234, 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+def copy_csv_to_postgresql(table_name: str, pg_conn: _connection):
+    """Перенос данных из csv файла в базу postgresql."""
+
+    with open(f"{table_name}.csv", "r") as f:
+        pg_cur = pg_conn.cursor()
+        pg_cur.copy_from(f, table_name, null="", sep="|")
+
+
+if __name__ == "__main__":
+    with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg2.connect(
+            **dsl, cursor_factory=DictCursor
+    ) as pg_conn:
+        table_names = (
+            "person",
+            "genre",
+            "film_work",
+            "person_film_work",
+            "genre_film_work",
+        )
+        sqlite_cur = sqlite_conn.cursor()
+        for table_name in table_names:
+            sqlite_cur.execute(f"select * from {table_name}")
+            save_csv(table_name, sqlite_cur.fetchall())
+            copy_csv_to_postgresql(table_name, pg_conn)
+            remove(f"{table_name}.csv")
